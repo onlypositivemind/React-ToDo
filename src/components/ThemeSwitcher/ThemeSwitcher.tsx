29bea2e24ddd30df20c58664b02b3fdb8c582276
Evgenii <evgenypetrov5230@gmail.com>
2023-01-23 18:21:19 +0300

    ls for theme

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,12 +3,22 @@ import { ReactComponent as MoonIcon } from 'assets/moon.svg';
 import { ReactComponent as SunIcon } from 'assets/sun.svg';
 import s from './ThemeSwitcher.module.css';
 
+const defaultTheme = (): boolean => {
+	const ls = localStorage.getItem('themeDark');
+	if (ls) {
+		return JSON.parse(ls);
+	} else {
+		return false;
+	}
+};
+
 const ThemeSwitcher: React.FC = () => {
-	const [isDark, setDark] = useState(false);
+	const [isDark, setDark] = useState(defaultTheme());
 	const ThemeIcon = isDark ? SunIcon : MoonIcon;
 	
 	useEffect(() => {
 		document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+		localStorage.setItem('themeDark', `${isDark}`);
 	}, [isDark]);
 	
 	return (
